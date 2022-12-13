62f6e6f63d5075679f9f8f2bfaf2212abe7b9242
Lakshmi pavani <96370043+Lakshmipavani123@users.noreply.github.com>
2022-12-13 22:27:52 +0530

    n=int(input()) m=list(map(int,input().split())) k=len(m) c=0 s=0 for i in range((k//2)):     c+=m[i] for j in range((k//2),k):     s+=m[j] if s>c:     print(s-c) else:     print(c-s)

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,13 @@
+n=int(input())
+m=list(map(int,input().split()))
+k=len(m)
+c=0
+s=0
+for i in range((k//2)):
+    c+=m[i]
+for j in range((k//2),k):
+    s+=m[j]
+if s>c:
+    print(s-c)
+else:
+    print(c-s)
