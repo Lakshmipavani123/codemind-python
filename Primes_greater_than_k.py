79128576963b2d74b46cb9b788787211b4c65e7f
Lakshmi pavani <96370043+Lakshmipavani123@users.noreply.github.com>
2023-05-11 22:02:32 +0530

    def pri(n):     if n==1:         return False     for i in range(2,n):         if n%i==0:             return False     else:         return True n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if pri(i):         if i>=k:             c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+def pri(n):
+    if n==1:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in l:
+    if pri(i):
+        if i>=k:
+            c+=1
+print(c)
