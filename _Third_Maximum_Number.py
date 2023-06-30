15e8c016ab3148c51f6679e76dcecbd2c046b27c
Lakshmi pavani <96370043+Lakshmipavani123@users.noreply.github.com>
2023-06-30 12:19:34 +0530

    n=int(input()) m=list(map(int,input().split())) m.sort() m2=m[::-1] m1=[] for i in m2:     if i not in m1:         m1.append(i) #print(m1) #print(m1) #print(len(m)) if len(m1)>=3:     print(m1[2]) else:     print(max(m1))

---
+++ _Third_Maximum_Number.py
@@ -0,0 +1,15 @@
+n=int(input())
+m=list(map(int,input().split()))
+m.sort()
+m2=m[::-1]
+m1=[]
+for i in m2:
+    if i not in m1:
+        m1.append(i)
+#print(m1)
+#print(m1)
+#print(len(m))
+if len(m1)>=3:
+    print(m1[2])
+else:
+    print(max(m1))
